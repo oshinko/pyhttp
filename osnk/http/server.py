090aaabb8af33f92fd6f05bc4644bb6b19bcd561
Oshinko <osnk@renjaku.jp>
2018-09-24 19:11:23 +0900

    Initial commit

---
+++ osnk/http/server.py
@@ -0,0 +1,303 @@
+import asyncio
+import datetime
+import json
+import pathlib
+import re
+import sys
+import traceback
+from .utils import ContentStream, Headers, Request
+
+pyversion = '.'.join(str(x) for x in sys.version_info[:3])
+default_name = 'Python/' + pyversion
+
+
+class HTTPServer:
+    first_line = re.compile(r'([^ ]+) ([^ ]+) [^ ]+')
+
+    def __init__(self, *, name=default_name, host='0.0.0.0', port=80,
+                 newline=b'\r\n', charset='utf-8', headers=None, debug=False):
+        self.name = name
+        self.host = host
+        self.port = port
+        self.newline = newline
+        self.charset = charset
+        self.headers = Headers({'Server': self.name})
+        for k, v in Headers(headers):
+            self.headers[k] = v
+        self.handlers = []
+        self.debug = debug
+
+    async def _not_found(self):
+        content = json.dumps('404 Not Found').encode(self.charset)
+        return content, 404, {'Content-Type': 'application/json'}
+
+    def not_found(self, fn):
+        self._not_found = fn
+        return fn
+
+    async def _method_not_allowed(self):
+        content = json.dumps('405 Method Not Allowed').encode(self.charset)
+        return content, 405, {'Content-Type': 'application/json'}
+
+    def method_not_allowed(self, fn):
+        self._method_not_allowed = fn
+        return fn
+
+    async def _error(self):
+        content = json.dumps('500 Internal Server Error').encode(self.charset)
+        return content, 500, {'Content-Type': 'application/json'}
+
+    def error(self, fn):
+        self._error = fn
+        return fn
+
+    def route(self, regex, methods=['GET']):
+        if not regex.startswith('^'):
+            regex = '^' + regex
+        if not regex.endswith('$'):
+            regex += '$'
+        pattern = re.compile(regex)
+
+        def wrapper(fn):
+            for method in methods:
+                self.handlers.append((method.upper(), pattern, fn))
+
+        return wrapper
+
+    async def handle(self, reader, writer, method, path, query):
+        handler = None
+        raw_headers = []
+        async for line in reader:
+            line = line.strip()
+            if not line:
+                break
+            header = line.decode().split(':', 1)
+            if len(header) == 2:
+                name, value = header
+                name = name.strip()
+                value = value.strip()
+                try:
+                    value = int(value)
+                except ValueError:
+                    try:
+                        value = float(value)
+                    except ValueError:
+                        pass
+                raw_headers.append((name, value))
+        headers = Headers(raw_headers)
+        if 'Content-Length' in headers:
+            if isinstance(headers['Content-Length'], tuple):
+                content_length = headers['Content-Length'][0]
+            else:
+                content_length = headers['Content-Length']
+        else:
+            content_length = 0
+        content = bytearray()
+        if isinstance(content_length, int):
+            chunk = 1024
+            total = 0
+            while total < content_length:
+                content.extend(await reader.read(chunk))
+                total += chunk
+        if 'Host' in headers:
+            if isinstance(headers['Host'], tuple):
+                h = headers['Host'][0]
+            else:
+                h = headers['Host']
+            if ':' in h:
+                host = h.split(':')[0]
+            else:
+                host = s
+        else:
+            host = self.host
+        url = 'http', host, self.port, path, query
+        request = Request(method, url, headers, content)
+        resources = []
+        for x, y, z in self.handlers:
+            m = y.match(path)
+            if m:
+                resources.append((x, y, z, m.groups()))
+        for x, y, z, parts in resources:
+            if x == method:
+                handler = z(request, *parts)
+                break
+        if resources and not handler:
+            handler = self._method_not_allowed()
+        if handler:
+            resp = await handler
+            if resp is None:
+                resp = await self._not_found()
+        else:
+            resp = await self._not_found()
+        max_returns = 3
+        min_returns = 1
+        if not isinstance(resp, tuple):
+            resp = resp,
+        resp = resp[:max_returns]
+        returns = len(resp)
+        if returns < min_returns:
+            m = 'not enough values to unpack (expected {}, got {})'
+            raise ValueError(m.format(min_returns, returns))
+        for _ in range(max_returns - returns):
+            resp += None,
+        content, status, headers = resp
+        if not status:
+            status = 200
+        headers = Headers(headers)
+        for k, v in self.headers:
+            headers[k] = v
+        if 'Date' not in headers:
+            d = datetime.datetime.utcnow()
+            headers['Date'] = d.strftime('%a, %d, %b %Y %H:%M:%S GMT')
+        try:
+            stream = ContentStream(content, self.charset)
+            if 'Content-Type' not in headers and stream.content_type:
+                headers['Content-Type'] = stream.content_type
+            headers['Content-Length'] = stream.content_length
+        except ValueError:
+            if 'Content-Type' not in headers:
+                raise ValueError('content-type is required')
+            stream = content
+        if self.debug:
+            print('[{}] "{} {}" {}'.format(
+                datetime.datetime.now(), method,
+                path + '?' + query if query else path, status))
+        writer.write('HTTP/1.1 {}'.format(status).encode(self.charset))
+        writer.write(self.newline)
+        for name, value in headers:
+            writer.write(name.encode(self.charset))
+            writer.write(': '.encode(self.charset))
+            writer.write(str(value).encode(self.charset))
+            writer.write(self.newline)
+        writer.write(self.newline)
+        await writer.drain()
+        async with stream as s:
+            try:
+                while True:
+                    b = await s.__aiter__().__anext__()
+                    writer.write(b)
+                    await writer.drain()
+            except StopAsyncIteration:
+                pass
+
+    async def callback(self, reader, writer):
+        try:
+            first = await reader.readline()
+            m = self.first_line.search(first.decode())
+            if m:
+                try:
+                    method, uri = m.groups()
+                    method = method.upper()
+                    parts = uri.split('?', 1)
+                    if len(parts) == 1:
+                        path, = parts
+                        query = None
+                    else:
+                        path, query = parts
+                    await self.handle(reader, writer, method, path, query)
+                except Exception as e:
+                    r = await self._error()
+                    await self.write_response(writer, r)
+                    raise e
+        except Exception:
+            print(traceback.format_exc(), file=sys.stderr)
+        finally:
+            writer.close()
+
+    def start(self, loop=None):
+        if not loop:
+            loop = asyncio.get_event_loop()
+        self.loop = loop
+        coro = asyncio.start_server(self.callback, self.host, self.port,
+                                    loop=self.loop)
+        self._server = self.loop.run_until_complete(coro)
+
+    def close(self):
+        self._server.close()
+        self.loop.run_until_complete(self._server.wait_closed())
+
+
+if __name__ == '__main__':
+    port = sys.argv[1]
+    token = sys.argv[2] if len(sys.argv) > 2 else None
+    server = HTTPServer(port=port, debug=True)
+
+    import functools
+
+    def requires(token):
+        def decorator(fn):
+            @functools.wraps(fn)
+            async def wrapper(request, *args, **options):
+                if not token:
+                    return await fn(request, *args, **options)
+                elif 'authorization' in request.headers:
+                    scheme = 'Bearer'
+                    auth = request.headers['authorization']
+                    if not isinstance(auth, tuple):
+                        auth = auth,
+                    for a in auth:
+                        a = a.strip()
+                        try:
+                            s = a.lower().index(scheme.lower()) + len(scheme)
+                            _token = a[s:].strip()
+                        except ValueError:
+                            _token = a
+                        if _token == token:
+                            return await fn(request, *args, **options)
+                return None, 401, {'WWW-Authenticate': 'Bearer'}
+            return wrapper
+        return decorator
+
+    @server.route('/(.*)', methods=['GET'])
+    @requires(token)
+    async def get(request, path):
+        p = pathlib.Path.cwd() / path.replace('..', '')
+        if p.is_file():
+            return p
+        elif p.is_dir():
+            return sorted((x.name, x.stat().st_size) for x in p.iterdir())
+
+    @server.route('/(.*)', methods=['POST'])
+    @requires(token)
+    async def post(request, path):
+        p = pathlib.Path.cwd() / path.replace('..', '')
+        if not p.parent.exists():
+            p.parent.mkdir(parents=True, exist_ok=True)
+        with p.open('ab') as f:
+            f.write(request.content)
+        return None, 200
+
+    @server.route('/(.*)', methods=['DELETE'])
+    @requires(token)
+    async def delete(request, path):
+        p = pathlib.Path.cwd() / path.replace('..', '')
+        if p.is_file():
+            try:
+                p.unlink()
+                return None, 200
+            except FileNotFoundError:
+                pass
+        elif p.is_dir():
+            try:
+                p.rmdir()
+                return None, 200
+            except OSError:
+                return None, 403
+
+    @server.route('/(.*)', methods=['OPTIONS'])
+    async def options(request, path):
+        return None, 200, {'Allow': 'GET, POST, DELETE, OPTIONS',
+                           'Access-Control-Allow-Headers': 'Authorization'}
+
+    loop = asyncio.get_event_loop()
+    server.start(loop)
+    start = 'Serving HTTP on {host} port {port} (http://{host}:{port}) ...'
+    print(start.format(host=server.host, port=server.port))
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.close()
+        loop.close()
---
+++ osnk/http/utils.py
@@ -0,0 +1,229 @@
+import json
+import io
+import pathlib
+import urllib.request
+
+
+class ContentStream:
+    def __init__(self, content, charset='utf-8'):
+        if isinstance(content, str):
+            self.content_type = 'text/plain'
+            self.content = content.encode(charset)
+            self.content_length = len(self.content)
+        elif isinstance(content, (dict, list)):
+            self.content_type = 'application/json'
+            self.content = json.dumps(content).encode(charset)
+            self.content_length = len(self.content)
+        elif isinstance(content, (bytearray, bytes)):
+            self.content_type = 'application/octet-stream'
+            self.content = content
+            self.content_length = len(self.content)
+        elif isinstance(content, pathlib.Path):
+            self.content_type = 'application/octet-stream'
+            self.content = content
+            self.content_length = content.stat().st_size
+        elif content is None:
+            self.content_type = None
+            self.content = None
+            self.content_length = 0
+        else:
+            m = "invalid type '{}'"
+            raise ValueError(m.format(type(content).__name__))
+
+    async def __aenter__(self):
+        if isinstance(self.content, (bytearray, bytes)) or \
+                self.content is None:
+            self.stream = io.BytesIO(self.content)
+        elif isinstance(self.content, pathlib.Path):
+            self.stream = self.content.open('rb')
+        else:
+            m = "invalid type '{}'"
+            raise ValueError(m.format(type(self.content).__name__))
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        self.stream.close()
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        r = self.stream.read(1024)
+        if not r:
+            raise StopAsyncIteration
+        return r
+
+
+class Headers:
+    def __init__(self, keyvals=None):
+        if keyvals:
+            if isinstance(keyvals, (dict, list, set, tuple)):
+                self._keyvals = {}
+                if isinstance(keyvals, dict):
+                    pairs = keyvals.items()
+                else:
+                    pairs = keyvals
+                for k, v in pairs:
+                    key = k.lower()
+                    if isinstance(v, list):
+                        vals = v
+                    elif isinstance(v, (set, tuple)):
+                        vals = list(v)
+                    else:
+                        vals = [v]
+                    if key in self._keyvals:
+                        currentkey, currentvals = self._keyvals[key]
+                        currentvals.extend(vals)
+                    else:
+                        self._keyvals[key] = (k, vals)
+            elif isinstance(keyvals, self.__class__):
+                self._keyvals = dict(keyvals._keyvals)
+            else:
+                raise ValueError()
+        else:
+            self._keyvals = {}
+
+    def __setitem__(self, key, value):
+        if isinstance(value, (list, set, tuple)):
+            self._keyvals[key.lower()] = (key, value)
+        else:
+            self._keyvals[key.lower()] = (key, (value,))
+
+    def __getitem__(self, key):
+        r = self._keyvals.get(key.lower())
+        if r:
+            key, vals = r
+            c = len(vals)
+            if c == 1:
+                return vals[0]
+            elif c > 1:
+                return tuple(vals)
+        return None
+
+    def __contains__(self, item):
+        return isinstance(item, str) and item.lower() in self._keyvals
+
+    def __repr__(self):
+        params = {}
+        for key, (k, v) in self._keyvals.items():
+            if len(v) == 1:
+                params[k] = v[0]
+            else:
+                params[k] = v
+        return '{}({})'.format(self.__class__.__name__, params)
+
+    def _generator(self):
+        for key, (k, v) in self._keyvals.items():
+            for x in v:
+                yield k, x
+
+    def __iter__(self):
+        return self._generator()
+
+
+class Arguments:
+    def __init__(self, keyvals=None):
+        if keyvals:
+            if isinstance(keyvals, (dict, list, set, tuple)):
+                self._keyvals = {}
+                if isinstance(keyvals, dict):
+                    pairs = keyvals.items()
+                else:
+                    pairs = keyvals
+                for key, v in pairs:
+                    if isinstance(v, list):
+                        vals = v
+                    elif isinstance(v, (set, tuple)):
+                        vals = list(v)
+                    else:
+                        vals = [v]
+                    if key in self._keyvals:
+                        self._keyvals[key].extend(vals)
+                    else:
+                        self._keyvals[key] = vals
+            elif isinstance(keyvals, self.__class__):
+                self._keyvals = dict(keyvals._keyvals)
+            else:
+                raise ValueError()
+        else:
+            self._keyvals = {}
+
+    def __setitem__(self, key, value):
+        self._keyvals[key] = value
+
+    def __getitem__(self, key):
+        vals = self._keyvals.get(key)
+        if vals:
+            c = len(vals)
+            if c == 1:
+                return vals[0]
+            elif c > 1:
+                return vals
+        return None
+
+    def __contains__(self, item):
+        return isinstance(item, str) and item in self._keyvals
+
+    def __repr__(self):
+        params = {}
+        for key, vals in self._keyvals.items():
+            if len(vals) == 1:
+                params[key] = vals[0]
+            else:
+                params[key] = vals
+        return '{}({})'.format(self.__class__.__name__, params)
+
+    def _generator(self):
+        for key, vals in self._keyvals.items():
+            for x in vals:
+                yield key, x
+
+    def __iter__(self):
+        return self._generator()
+
+
+class Request:
+    def __init__(self, method, url, headers, content):
+        self.method = method
+        self.url = url
+        if isinstance(url, (list, tuple)):
+            length = len(url)
+            maxlen = 5
+            if length >= maxlen:
+                self.url = url[:maxlen]
+            else:
+                self.url = url
+                for _ in range(maxlen - length):
+                    self.url += (None,)
+        if isinstance(headers, Headers):
+            self.headers = headers
+        else:
+            self.headers = Headers(headers)
+        self.content = content
+
+    @property
+    def args(self):
+        if not hasattr(self, '_args'):
+            shceme, host, port, path, query = self.url
+            self._args = Arguments(urllib.parse.parse_qs(query))
+        return self._args
+
+    @property
+    def form(self):
+        if not hasattr(self, '_form'):
+            if isinstance(self.content, str):
+                s = self.content
+            elif isinstance(self.content, (bytearray, bytes)):
+                try:
+                    s = self.content.decode()
+                except UnicodeDecodeError:
+                    s = None
+            else:
+                raise ValueError()
+            self._form = Arguments(urllib.parse.parse_qs(s))
+        return self._form
+
+    def __repr__(self):
+        return "{}('{}', {}, {}, {})".format(self.__class__.__name__,
+                                             self.method, self.url,
+                                             self.headers, self.content)
